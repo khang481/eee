de2b0e892366a486563e73604916f526a7a36c5e
HuBo2006 <banhbest@gmail.com>
2026-04-01 22:07:51 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <raylib.h>
+int main(){
+    Color darkBlue = {44, 44, 127, 255};
+    InitWindow(300, 600, "raylib Tetris");
+    SetTargetFPS(60);
+    while(WindowShouldClose() == false){
+        BeginDrawing();
+        ClearBackground(darkBlue);
+
+        EndDrawing();
+    }
+    CloseWindow();
+}
