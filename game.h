b51ae1367e794bdc9c26e13f4fdb22c485f43a91
khang481 <24021282@vnu.edu.vn>
2026-04-07 18:03:23 +0700

    Add files via upload

---
+++ game.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "grid.h"
+#include "blocks.cpp"
+using namespace std;
+
+class Game
+{
+public:
+    Game();
+    ~Game();
+    void Draw();
+    void HandleInput();
+    void MoveBlockDown();
+    bool gameOver;
+    int score;
+    Music music;
+
+private:
+    void MoveBlockLeft();
+    void MoveBlockRight();
+    Block GetRandomBlock();
+    vector<Block> GetAllBlocks();
+    bool IsBlockOutside();
+    void RotateBlock();
+    void LockBlock();
+    bool BlockFits();
+    void Reset();
+    void UpdateScore(int linesCleared, int moveDownPoints);
+    Grid grid;
+    vector<Block> blocks;
+    Block currentBlock;
+    Block nextBlock;
+    Sound rotateSound;
+    Sound clearSound;
+};
