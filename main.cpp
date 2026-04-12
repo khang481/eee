38fe9d3420758c7fffda3400044c58c18029cd2a
HuBo2006 <banhbest@gmail.com>
2026-04-13 00:34:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,8 +18,9 @@ int main(){
     InitWindow(500, 620, "raylib Tetris");
     SetTargetFPS(60);
 
-    Texture2D background = LoadTexture("Resources/background.jpg");
-    Texture2D menu = LoadTexture("Resources/menu.png");
+    Texture2D background = LoadTexture("background.jpg");
+    Texture2D menu = LoadTexture("menu.png");
+    Texture2D setting = LoadTexture("setting.png");
 
     Font font = LoadFontEx("Resources/monogram.ttf", 64, 0, 0);
 
@@ -30,9 +31,57 @@ int main(){
     while(WindowShouldClose() == false){
         UpdateMusicStream(game.music);
 
+        Vector2 mousePoint = GetMousePosition();
+        Rectangle settingBtn = {440, 560, 45, 45};
+
+        if (IsKeyPressed(KEY_SPACE)) {
+            if (!inMenu && !game.gameOver) {
+                game.inSettings = !game.inSettings;
+
+            if (game.inSettings) game.isPaused = true;
+            else game.isPaused = false;
+    }
+}
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mousePoint, settingBtn)){
+            game.inSettings = !game.inSettings;
+
+            if (game.inSettings && !inMenu && !game.gameOver){
+                game.isPaused = true;
+            }
+        } else if (game.inSettings){
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+                Rectangle MusicBtn = {125, 200, 250, 50};
+                Rectangle sfxBtn = {125, 280, 250, 50};
+                Rectangle continueBtn = {125, 360, 250, 50};
+                Rectangle returnBtn = {125, 440, 250, 50};
+
+                if (CheckCollisionPointRec(mousePoint, MusicBtn)){
+                    game.musicEnabled = !game.musicEnabled;
+
+                    if (game.musicEnabled) ResumeMusicStream(game.music);
+                    else PauseMusicStream(game.music);
+                }
+                if (CheckCollisionPointRec(mousePoint, sfxBtn)){
+                    game.sfxEnabled = !game.sfxEnabled;
+                }
+                if (CheckCollisionPointRec(mousePoint, continueBtn)){
+                    game.inSettings = false;
+                    game.isPaused = false;
+                }
+                if (!inMenu){
+                    if (CheckCollisionPointRec(mousePoint, returnBtn)){
+                        game.inSettings = false;
+                        game.isPaused = false;
+                        inMenu = true;
+                        game.Reset();
+                    }
+                }
+            }
+        } else {
+
         if (inMenu){
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-                Vector2 mousePoint = GetMousePosition();
                 Rectangle startBtn = {125, 450, 250, 60};
 
                 if (CheckCollisionPointRec(mousePoint, startBtn)){
@@ -46,27 +95,8 @@ int main(){
         }
         }
 
-        if (game.isPaused){
-            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-                Vector2 mousePoint = GetMousePosition();
-
-                Rectangle continueBtn = {125, 250, 250, 50};
-                Rectangle newGameBtn = {125, 350, 250, 50};
-
-                if (CheckCollisionPointRec(mousePoint, continueBtn)){
-                    game.isPaused = false;
-                }
-                if (CheckCollisionPointRec(mousePoint, newGameBtn)){
-                    game.Reset();
-                    game.isPaused = false;
-                }
-            }
-        }
-
         if (game.gameOver){
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-                Vector2 mousePoint = GetMousePosition();
-
                 Rectangle newGameBtn = {125, 380, 250, 50};
 
                 if (CheckCollisionPointRec(mousePoint, newGameBtn)){
@@ -74,6 +104,7 @@ int main(){
                 }
             }
         }
+    }
 
         BeginDrawing();
         DrawTexture(background, 0, 0, WHITE);
@@ -100,18 +131,8 @@ int main(){
 
             DrawTextEx(font, scoreText, {320 + (170 - textSize.x)/2, 65}, 38, 2, WHITE);
             DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, darkBlue);
+
             game.Draw();
-
-        if (game.isPaused){
-            DrawRectangle(0, 0, 500, 620, Fade(BLACK, 0.6f));
-            Rectangle continueBtn = {125, 250, 250, 50};
-            DrawRectangleRec(continueBtn, LIGHTGRAY);
-            DrawTextEx(font, "Continue", {165, 255}, 38, 2, BLACK);
-
-            Rectangle newGameBtn = {125, 350, 250, 50};
-            DrawRectangleRec(newGameBtn, LIGHTGRAY);
-            DrawTextEx(font, "New Game", {165, 355}, 38, 2, BLACK);
-        }
 
         if (game.gameOver){
             DrawRectangle(0, 0, 500, 620, Fade(BLACK, 0.8f));
@@ -122,13 +143,39 @@ int main(){
 
             Rectangle newGameBtn = {125, 380, 250, 50};
             DrawRectangleRec(newGameBtn, LIGHTGRAY);
-            DrawTextEx(font, "New Game", {165, 390}, 38, 2, BLACK);
+            DrawTextEx(font, "New Game", {170, 390}, 38, 2, BLACK);
         }
+        }
+
+        DrawTexture(setting, 440, 560, WHITE);
+
+        if (game.inSettings){
+            DrawRectangle(0, 0, 500, 620, Fade(BLACK, 0.9f));
+            DrawTextEx(font, "Settings", {150, 100}, 50, 2, WHITE);
+
+            Rectangle MusicBtn = {125, 200, 250, 50};
+            DrawRectangleRounded(MusicBtn, 0.2, 5, game.musicEnabled ? GREEN : GRAY);
+            DrawTextEx(font, game.musicEnabled ? "Music: ON" : "Music: OFF", {160, 210}, 38, 2, BLACK);
+
+            Rectangle sfxBtn = {125, 280, 250, 50};
+            DrawRectangleRounded(sfxBtn, 0.2, 5, game.sfxEnabled ? GREEN : GRAY);
+            DrawTextEx(font, game.sfxEnabled ? "SFX: ON" : "SFX: OFF", {180, 290}, 38, 2, BLACK);
+
+            Rectangle continueBtn = {125, 360, 250, 50};
+            DrawRectangleRounded(continueBtn, 0.2, 5, LIGHTGRAY);
+            DrawTextEx(font, "Continue", {175, 370}, 38, 2, BLACK);
+
+            if (!inMenu){
+                Rectangle returnBtn = {125, 440, 250, 50};
+                DrawRectangleRounded(returnBtn, 0.2, 5, LIGHTGRAY);
+                DrawTextEx(font, "Return Menu", {145, 450}, 38, 2, BLACK);
+            }
         }
         EndDrawing();
     }
     UnloadTexture(background);
     UnloadTexture(menu);
+    UnloadTexture(setting);
 
     CloseWindow();
     return 0;
