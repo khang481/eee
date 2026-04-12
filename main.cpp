74ef149809e8aebdcf2d0ea8fbd246a91e6ef6d5
HuBo2006 <banhbest@gmail.com>
2026-04-12 22:02:12 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,16 +18,32 @@ int main(){
     InitWindow(500, 620, "raylib Tetris");
     SetTargetFPS(60);
 
+    Texture2D background = LoadTexture("background.jpg");
+    Texture2D menu = LoadTexture("menu.png");
+
     Font font = LoadFontEx("Resources/monogram.ttf", 64, 0, 0);
 
     Game game = Game();
 
+    bool inMenu = true;
+
     while(WindowShouldClose() == false){
         UpdateMusicStream(game.music);
-        game.HandleInput();
 
-        if (EventTriggered(0.4) && game.gameOver == false && game.isPaused == false){
-            game.MoveBlockDown();
+        if (inMenu){
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+                Vector2 mousePoint = GetMousePosition();
+                Rectangle startBtn = {125, 450, 250, 60};
+
+                if (CheckCollisionPointRec(mousePoint, startBtn)){
+                    inMenu = false;
+                }
+            }
+        } else {
+                game.HandleInput();
+                if (EventTriggered(0.35) && game.gameOver == false && game.isPaused == false){
+                    game.MoveBlockDown();
+        }
         }
 
         if (game.isPaused){
@@ -60,20 +76,33 @@ int main(){
         }
 
         BeginDrawing();
-        ClearBackground(darkBlue);
-        DrawTextEx(font, "Score", {355, 15}, 38, 2, WHITE);
-        DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
+        DrawTexture(background, 0, 0, WHITE);
 
-        DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
+        if (inMenu){
+            DrawTexture(menu, 0, 0, WHITE);
 
-        char scoreText[10];
-        sprintf(scoreText, "%d", game.score);
-        Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+            const char* creatorText = "with HuBo & Xahoy";
+            Vector2 textSize = MeasureTextEx(font, creatorText, 30, 2);
+            DrawTextEx(font, creatorText, {(500 - textSize.x)/2, 380}, 30, 2, WHITE);
 
-        DrawTextEx(font, scoreText, {320 + (170 - textSize.x)/2, 65}, 38, 2, WHITE);
-        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
+            Rectangle startBtn = {125, 450, 250, 60};
+            DrawRectangleRounded(startBtn, 0.2, 5, DARKBLUE);
+            DrawTextEx(font, "START GAME", {150, 460}, 40, 2, WHITE);
+        } else {
+            DrawTextEx(font, "Score", {355, 15}, 38, 2, WHITE);
+            DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
 
-        if(game.isPaused){
+            DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, darkBlue);
+
+            char scoreText[10];
+            sprintf(scoreText, "%d", game.score);
+            Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+
+            DrawTextEx(font, scoreText, {320 + (170 - textSize.x)/2, 65}, 38, 2, WHITE);
+            DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, darkBlue);
+            game.Draw();
+
+        if (game.isPaused){
             DrawRectangle(0, 0, 500, 620, Fade(BLACK, 0.6f));
             Rectangle continueBtn = {125, 250, 250, 50};
             DrawRectangleRec(continueBtn, LIGHTGRAY);
@@ -82,8 +111,6 @@ int main(){
             Rectangle newGameBtn = {125, 350, 250, 50};
             DrawRectangleRec(newGameBtn, LIGHTGRAY);
             DrawTextEx(font, "New Game", {165, 355}, 38, 2, BLACK);
-        } else {
-        game.Draw();
         }
 
         if (game.gameOver){
@@ -97,7 +124,12 @@ int main(){
             DrawRectangleRec(newGameBtn, LIGHTGRAY);
             DrawTextEx(font, "New Game", {165, 390}, 38, 2, BLACK);
         }
+        }
         EndDrawing();
     }
+    UnloadTexture(background);
+    UnloadTexture(menu);
+
     CloseWindow();
+    return 0;
 }
